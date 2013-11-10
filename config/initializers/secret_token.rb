0ce5c0d1270ee1afcbeb89e5172170c715bf68c6
suras <msurendar@outlook.com>
2013-11-10 17:24:35 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Geotagz::Application.config.secret_key_base = '700bf1885798a332952a8f879aa278db9c121a9e2fe2684160b48d7606d6c337b762aa5a729e84cea0e2f17c1706b836a65306a62fcf9290a2d8755a722be896'
